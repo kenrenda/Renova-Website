00aa97fdbf3f68ad3e960702d6d808836a7cf6b4
KenNyirenda <141168807+KenNyirenda@users.noreply.github.com>
2025-10-19 18:54:38 +0200

    nav half way

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ['./*.html,js,php'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },  
+    extend: {
+      colors:{
+        renovaBlue: 'hsl(259, 73%, 28%)',
+        renovaGreen: 'hsl(70, 93%, 44%)',
+        renovaWhite: 'hsl(0, 0%, 85%)',
+        renovaBrightBlue: 'hsl(259, 80%, 39%)',
+      },
+      backgroundImage: {
+        'renova-gradient': 'linear-gradient(90deg, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
